8f2c7d7a4df734bd5ef42d13430e2b96d7a87126
zhaomanzhou <zmzsstreet@163.com>
2021-04-19 13:26:48 +0800

    更新

--- idf-proxy/src/main/java/com/idofast/proxy/ProxyApplication.java
+++ idf-proxy/src/main/java/com/idofast/proxy/ProxyApplication.java
@@ -1,5 +1,8 @@
 package com.idofast.proxy;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,11 +14,21 @@ import java.util.TimeZone;
  * @createTime 2021/4/12 1:32 下午
  */
 @SpringBootApplication
-public class ProxyApplication
+@Slf4j
+public class ProxyApplication implements CommandLineRunner
 {
+    @Value("${proxy.local.host}")
+    private String localHost;
+
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
         //需要接受args，如果不加载不了自定义配置
         SpringApplication.run(ProxyApplication.class,args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception
+    {
+        log.info("当前服务器名称:{}", localHost);
     }
 }
